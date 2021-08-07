5694676329a9f71588c93a3e4698a20423578b5e
idumlija <67471816+idumlija@users.noreply.github.com>
2021-08-07 17:22:51 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,19 +2,4 @@
 
 INTERNET_STATUS="UNKNOWN"
 TIMESTAMP=`date +%s`
-while [ 1 ]
- do
-    ping -c 1 -W 0.7 8.8.4.4 > /dev/null 2>&1
-    if [ $? -eq 0 ] ; then
-        if [ "$INTERNET_STATUS" != "UP" ]; then
-            echo "UP   `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
-            INTERNET_STATUS="UP"
-        fi
-    else
-        if [ "$INTERNET_STATUS" = "UP" ]; then
-            echo "DOWN `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP))";
-            INTERNET_STATUS="DOWN"
-        fi
-    fi
-    sleep 1
- done;
+echo $TIMESTAMP
